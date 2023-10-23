f657c2d653ad77861b8dd39a47f42c0d072337c3
misha sharipov <114057948+iwgth@users.noreply.github.com>
2023-10-23 21:34:01 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 # адреса та порт сервера
 server_address = ('localhost', 12345)
-# створення сокету та з'єднання з сервером
+# створення сокету та з'єднання з сервером 2
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
