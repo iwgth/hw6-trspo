bed2f1337173e911fe03f917b0bc749c8502be1b
iwgth <114057948+iwgth@users.noreply.github.com>
2023-10-23 21:32:37 +0300

    first commit1

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+
+# адреса та порт сервера
+server_address = ('localhost', 12345)
+# створення сокету та з'єднання з сервером
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(server_address)
+
+# відправка та отримання повідомлень
+for i in range(1, 101):
+    message = f"отримане повідомлення від клієнта {i}".encode()
+    message_length = len(message).to_bytes(4, byteorder='big')
+    client_socket.send(message_length + message)
+
+    # отримуємо відповіді від сервера
+    response_length_bytes = client_socket.recv(4)
+    response_length = int.from_bytes(response_length_bytes, byteorder='big')
+    response = client_socket.recv(response_length)
+    decoded_response = response.decode()
+    print(f"отримана відповідь від сервера: {decoded_response}")
+
+client_socket.close()
---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+
+# адреса та порт сервера
+server_address = ('localhost', 12345)
+# створення сокету та очікування з'єднання
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(server_address)
+server_socket.listen(1)
+print("очікуємо підключення...")
+client_socket, client_address = server_socket.accept()
+print(f"підключення встановлено з {client_address}")
+
+# отримали та відправили повідомлення
+for i in range(1, 101):
+    # отримуємо повідомлення від клієнта
+    message_length_bytes = client_socket.recv(4)
+    message_length = int.from_bytes(message_length_bytes, byteorder='big')
+    message = client_socket.recv(message_length)
+    decoded_message = message.decode()
+    print(f"отримано від клієнта: {decoded_message}")
+    # відправляємо відповідь клієнту
+    response = f"отримана відповідь від сервера на повідомлення номер {i}".encode()
+    response_length = len(response).to_bytes(4, byteorder='big')
+    client_socket.send(response_length + response)
+
+client_socket.close()
+server_socket.close()
